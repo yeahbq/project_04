142ca5073a09b0b3d714835665bc0bcbcb8aec36
Barrett Quan <21barrett@gmail.com>
2017-03-02 10:42:09 -0800

    removed extra files included from pull request

--- models/example.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose')
-
-var ExampleSchema = new mongoose.Schema({
-  name: String,
-  createdAt: { type: Date, default: Date.now }
-})
-
-module.exports = mongoose.model('Example', ExampleSchema)
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Virtual-Pests Homepage'})
-})
-
-module.exports = router
